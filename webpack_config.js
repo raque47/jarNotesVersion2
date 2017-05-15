f61da2e2b7fcea476d0b2a53e2778ea17e14cc9c
raque47 <raquelchava_94@hotmail.com>
2017-05-15 10:22:47 -0600

    Cambios de configuración

--- webpack_config.js
+++ webpack_config.js
@@ -1,6 +1,6 @@
 const path = require('path')
 
-module.exports ={
+module.exports = {
     context: __dirname,
     entry: 'js/ClientApp.js',
     devtool: 'eva;',
@@ -14,13 +14,35 @@ module.exports ={
     stats: {
         colors: true,
         reasons: true,
-        chunks: true
+        chunks: true  //para partir el codigo en diferentes files y no solo uno
     },
     module: {
         rules: [
             {
+                enforce: 'pre', //preloader
+                test: /\.js$/,
+                loader: 'eslint-loader',
+                exclude: /node_modules/
+
+            },
+            {
+                // exclude: /node_modules/, // no corra nodemodules por babel
+                // include: path resolve(__dirname, 'js'),
                 test: /\.js$/,
                 loader: 'babel-loader'
+            },
+            {
+                 test: /\.css$/,
+                 use:[
+                     'style-loader', //inject your style in your bundle css pega los archovs al html y lo lee de izquierda a derecha 
+
+                     {
+                        "loader": 'css-loader', 
+                        option: {
+                         url:'false'    
+                        }// hacer que pueda importar y reocnocer los archivos .css
+                     }
+                 ]
             }
         ]
     },
