ce4b32a380ac3649d3daf75e58bfbc22e777da9e
lariac <lariac24@gmail.com>
2017-05-17 16:45:37 -0600

    Modificacion del componente de botones del menu e inicio de creación del componente referente a la edición de notas

--- src/MenuButtons.jsx
+++ src/MenuButtons.jsx
@@ -14,5 +14,4 @@ render(){
   },
 });
 
-
 module.exports = MenuButtons;  
--- src/NotesEditionPanel.jsx
+++ src/NotesEditionPanel.jsx
@@ -1,1 +1,20 @@
 const React = require('react');
+
+const NotesEditionPanel = React.createClass({
+   render(){
+     return (
+        <div className="form-group noteText">
+            <input type="text" placeholder="Title" className="titleNoteControl" id="titleNote" />
+            <textarea className="form-control noteContentFont" rows="5" id="noteContent" placeholder="Write a new note..."></textarea>
+            <div className="editionNotesControl">
+                <button className="editionButtonControls"><img src="../images/paint-button.svg" className="navBar__images" data-toggle="tooltip" data-placement="top" title="Font Color" /></button>
+                <button className="editionButtonControls"><img src="../images/upload-button.svg" className="navBar__images" data-toggle="tooltip" data-placement="top" title="Load Image" /></button>
+                <button id="addNoteButton" className="editionButtonControls"><span className="editionTextControl" data-toggle="tooltip" data-placement="top" title="Add Note">Save Note</span></button>
+            </div>
+        </div>
+    );
+  },
+});
+
+
+module.exports = NotesEditionPanel;  
