d502392313a85446c0fb2011c5cc39f6feeaa936
raque47 <raquelchava_94@hotmail.com>
2017-05-18 14:57:34 -0600

    Cambio de rutas

--- src/Containers/App.jsx
+++ src/Containers/App.jsx
@@ -21,59 +21,20 @@ const App = React.createClass({
   render() {
     return (
       <div>
-        <NavBar/>
+        <NavBar
+        
+        />
         <div className="container-fluid container-fluid-fix ">
           <div className="row edition secondContainer">
-            <div className="col-md-1 firstElement">
-              <EditionBar />
-            </div>
-            <div className="col-md-8 col-sm-11  col-xs-12 secondElement">
-              <NotesEditionPanel
-                         
-              
-              />
-              
-            </div>
-
-
-        </div>
-
-
-
-
+              <div className="col-md-1 firstElement">
+                <EditionBar />
+              </div>
+              <div className="col-md-8 col-sm-11  col-xs-12 secondElement">
+                <NotesEditionPanel/> 
+              </div>
+          </div>
           <div className="col-md-3 col-sm-11 col-xs-12 thirthElement">
-            <div id="panelInformation" className="infoPanel">
-
-              
-              { /* TITLE COMPONENT <div>
-                  <h2 id="informationPanelTittle" className="fontFamily"> Notes</h2>
-                </div> */}
-
-            { /* SEARCH COMPONENT  <form id="searchForm" className="navbar-form searchFormHidden" role="search">
-                  <div id="searchForm__input" className="input-group searchFormHidden">
-                    <input type="text" className="form-control searchFormHidden" placeholder="Search" name="srch-term" id="srch-term" />
-                    <div className="input-group-btn">
-                      <button className="btn btn-default" type="submit"><img src={require('./images/search-button.svg')} className="searchButtonInformationPanel" /> </button>
-                    </div>
-                  </div>
-                </form> */ }
-
-              {/* LISTA DE NOTAS/FOLDERS/TAGS */}
-              <InformationContainer/>
-     
-              {/* ADD FOLDER/TAG COMPONENT <div id="addNewElementForm" className="addNewElementPanel addNewElementFormHidden">
-
-                  <div className="addNewElementTittleType">
-                    <h4 id="" className="fontFamily"> Add a new tag</h4>
-                  </div>
-                  <div className="formAddNewElement">
-                    <input id="addNewElementTittle" type="text" placeholder="Title" className="titleNoteControl" />
-                    <textarea id="addNewElementContent" className="textAreaAddElement"></textarea>
-                    <button id="addNoteButton" className="editionButtonControls addNewElementButton"><span className="editionTextControl">Add Tag</span></button>
-                  </div>
-                </div> */}
-              
-            </div>
+             <InformationPanel/> 
           </div>
         </div>
       </div>
