e1e83547ff597767e6117bdfd579bd5faaecd7cb
lariac <lariac24@gmail.com>
2017-05-18 22:46:00 -0600

    Cambios para responsive

--- src/Components/MenuButtons.jsx
+++ src/Components/MenuButtons.jsx
@@ -6,7 +6,7 @@ const MenuButtons = React.createClass({
         <div className="inlineContainer">
           <button id="" className="navBar__button buttons"><img src={require(`../images/${this.props.mainButton}`)} className="navBar__images" data-toggle="tooltip" data-placement="top" title={this.props.tooltipName} /><span className="navElementSpanVisible">{this.props.buttonName}</span></button>
           <div id="" className="hiddenButtons">
-            <button id="" className="hiddenButtons__button buttons"><img src={require(`../images/${this.props.searchIcon}`)} className="navBar__hiddenImages" /> <span className=""> {this.props.searchText}</span></button>
+            <button id="" className="hiddenButtons__button buttons"><img src={require(`../images/${this.props.searchIcon}`)} className="navBar__hiddenImages" /> <span className="buttonFontHidden"> {this.props.searchText}</span></button>
             <a href="#addNewElementForm" id="" className="hiddenButtons__button buttons"><img src={require(`../images/${this.props.hiddenIcon}`)} className="navBar__hiddenImages" /> <span className="buttonFontHidden"> {this.props.text}</span></a>
           </div>
         </div>
