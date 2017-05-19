3e1a999541a094b8a02c53becff891279df2237d
lariac <lariac24@gmail.com>
2017-05-19 15:55:21 -0600

    Inicio de creación de eventos en el menú

--- src/Components/MenuButtons.jsx
+++ src/Components/MenuButtons.jsx
@@ -2,15 +2,27 @@ const React = require('react');
 // const bootstrap = require('../../node_modules/bootstrap-sass/assets/stylesheets/_bootstrap.scss');
 const style= require('../scss/pages/_MenuButtonsStyle.scss');
 //const _menuButtonsStyle = require('../scss/pages/_MenuButtonsStyle.scss');
-
+{/*this.props.visibility*/} 
 const MenuButtons = React.createClass({
+  getInitialState () {
+    return {
+      searchTerm: ''
+    }
+  },
+    activateHiddenButtons(event) {
+      var idButton = event.target.id;
+      this.props.onClickMainButton(idButton);
+     // this.props.onClick(event.target.id);
+      console.log(event.target.id);
+   //   this.setState({ searchTerm: event.target.value })
+  },
   render() {
     return (
         <div className="inlineContainer">
-          <button id="" className="navBar__button buttons"><img src={require(`../images/${this.props.mainButton}`)} className="navBar__images" data-toggle="tooltip" data-placement="top" title={this.props.tooltipName} /><span className="navElementSpanVisible">{this.props.buttonName}</span></button>
-          <div id="" className="hiddenButtons">
-            <button id="" className="hiddenButtons__button buttons"><img src={require(`../images/${this.props.searchIcon}`)} className="navBar__hiddenImages" /> <span className="buttonFontHidden"> {this.props.searchText}</span></button>
-            <a href="#addNewElementForm" id="" className="hiddenButtons__button buttons"><img src={require(`../images/${this.props.hiddenIcon}`)} className="navBar__hiddenImages" /> <span className="buttonFontHidden"> {this.props.text}</span></a>
+          <button id={this.props.id} onClick= {this.activateHiddenButtons}  className="navBar__button buttons displayType"><img src={require(`../images/${this.props.mainButton}`)} className="navBar__images" data-toggle="tooltip" data-placement="top" title={this.props.tooltipName} /><span className="navElementSpanVisible">{this.props.buttonName}</span></button>
+          <div id="" className={this.props.displayEvent}  >
+            <button id="" className='hiddenButtons__button buttons displayType'><img src={require(`../images/${this.props.searchIcon}`)} className="navBar__hiddenImages" /> <span className="buttonFontHidden"> {this.props.searchText}</span></button>
+            <a href="#addNewElementForm" id="" className='hiddenButtons__button buttons displayType' ><img src={require(`../images/${this.props.hiddenIcon}`)} className="navBar__hiddenImages" /> <span className="buttonFontHidden"> {this.props.text}</span></a>
           </div>
         </div>
     );
--- src/Containers/MenuButtonsContainer.jsx
+++ src/Containers/MenuButtonsContainer.jsx
@@ -3,14 +3,40 @@ const MenuButtons= require('../Components/MenuButtons');
 const DropDown= require('../Components/DropDown');
 
 const MenuButtonsContainer = React.createClass({
+    getInitialState () {
+    return {
+      isActiveNotes: false,
+      isActiveTags:false,
+      isActiveFolders:false,
+      idButton: ''
+    }
+  },
+    showHiddenButtons(id) {
+   
+      this.setState({ idButton: id })
+     /*this.setState({
+        id:id
+     }); */
+      console.log('el target tiene: ' + idButton);
+  },
      render() {
         const searchIcon = "search-button.svg";
         const viewIcon="view-button.svg";
         return (
             <div >
-                <MenuButtons mainButton="notes-button.svg" buttonName="Notes" searchIcon={searchIcon} hiddenIcon={viewIcon} searchText="Search Note" text="View Notes" />
-                <MenuButtons mainButton="tag-button.svg" buttonName="Tags" searchIcon={searchIcon} hiddenIcon="add-tag-button.svg" searchText="Search Tag" text="Add Tag" />
-                <MenuButtons mainButton="folder-button.svg" buttonName="Folders" searchIcon={searchIcon} hiddenIcon="add-folder-button.svg" searchText="Search Folder" text="Add Folder" />    
+                <MenuButtons
+                  /*  onClick = {this.showHiddenButtons}  */
+                    id="notesButton"
+                    onClickMainButton ={this.showHiddenButtons} 
+                    displayEvent="visibleControl" 
+                    mainButton="notes-button.svg" 
+                    buttonName="Notes"
+                    searchIcon={searchIcon} 
+                    hiddenIcon={viewIcon} 
+                    searchText="Search Note" 
+                    text="View Notes"/>
+                <MenuButtons id = "tagsButton"  onClickMainButton ={this.showHiddenButtons}  displayEvent="invisibleControl" mainButton="tag-button.svg" buttonName="Tags" searchIcon={searchIcon} hiddenIcon="add-tag-button.svg" searchText="Search Tag" text="Add Tag" />
+                <MenuButtons id = "foldersButton"  onClickMainButton ={this.showHiddenButtons}  displayEvent="invisibleControl" mainButton="folder-button.svg" buttonName="Folders" searchIcon={searchIcon} hiddenIcon="add-folder-button.svg" searchText="Search Folder" text="Add Folder" />    
                 <DropDown userIcon="user-button.svg" item1="Settings" item2="Log Out" />        
             </div>
         );
