c5d7e429d893f43edf4a721d72a369648954ff45
raque47 <raquelchava_94@hotmail.com>
2017-05-25 13:52:48 -0600

    Se agregan variables a dotenv

--- api/index.js
+++ api/index.js
@@ -1,6 +1,5 @@
 const express=require("express");
 
-const mongoose = require('mongoose');
 const port = process.env.PORT || 4000;
 
 var app = express();
@@ -8,30 +7,6 @@ var app = express();
 
 const routes = require('./')
 
-
-
-require('dotenv').config();
-
-
-mongoose.connect(process.env.DB);
-
-mongoose.connection.on('connected', function () {  
-  console.log('Mongoose default connection open to ' + process.env.DB);
-}); 
-
-// If the connection throws an error
-mongoose.connection.on('error',function (err) {  
-  console.log('Mongoose default connection error: ' + err);
-}); 
-
-// When the connection is disconnected
-mongoose.connection.on('disconnected', function () {  
-  console.log('Mongoose default connection disconnected'); 
-});
-
-app.listen(port, ()=>{
-    console.log(`Express listening on port ${port}`);
-}); //Lleva 2 parametros, el primer parametro es el puerto donde correra el server y el segundo es el callback
 
 
 
