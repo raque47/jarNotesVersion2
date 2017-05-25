d48a39ea119773147e678b76567f718907690d2d
raque47 <raquelchava_94@hotmail.com>
2017-05-25 14:50:31 -0600

    Se agrega el scheme de tags

---
+++ api/models/Tag.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+// mongoose.Promise = global.Promise;
+
+const tagSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: 'The tag must have a name'
+    },
+});
+
+module.exports = mongoose.model('store', tagSchema);
