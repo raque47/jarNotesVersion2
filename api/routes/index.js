e0cbf3f8d8f7cbdf7c2de3477b3fba4615864f62
raque47 <raquelchava_94@hotmail.com>
2017-05-25 14:10:26 -0600

    Continuacion del api

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,3 +1,6 @@
+
+
+
 router.get('/', (req, res) =>{
     //res.send("Hello World!");
     res.json({  //No se puede hacer .json y .send simultaneamente porque por debajo estos implementan un res.send para no dejar el server guindando
